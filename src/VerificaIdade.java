506345c5288897bc46b10febf5002bbc95531364
NordligKrake <joaocoronetti86@gmail.com>
2024-09-01 19:19:21 -0300

    first commit

---
+++ src/VerificaIdade.java
@@ -0,0 +1,10 @@
+class VerificaIdade {
+    public static void main(String[] args) {
+        int idade = 17;
+        if (idade >= 18) {
+            System.out.println("A pessoa é maior de idade.");
+        } else {
+            System.out.println("A pessoa é menor de idade.");
+        }
+    }
+}
